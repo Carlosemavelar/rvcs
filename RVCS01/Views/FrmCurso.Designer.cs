f4565cd1b37e7391a31c3a370632cb780d01d4ff
carlos.emavelar <carlos.emavelar@ITQ0626049W10-1>
2020-11-13 10:34:17 -0300

    lista implementada

--- RVCS01/Views/FrmCurso.Designer.cs
+++ RVCS01/Views/FrmCurso.Designer.cs
@@ -28,14 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnListar = new System.Windows.Forms.Button();
+            this.lsbCursos = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // BtnListar
+            // 
+            this.BtnListar.Location = new System.Drawing.Point(70, 57);
+            this.BtnListar.Name = "BtnListar";
+            this.BtnListar.Size = new System.Drawing.Size(75, 44);
+            this.BtnListar.TabIndex = 0;
+            this.BtnListar.Text = "Listar";
+            this.BtnListar.UseVisualStyleBackColor = true;
+            this.BtnListar.Click += new System.EventHandler(this.BtnListar_Click);
+            // 
+            // lsbCursos
+            // 
+            this.lsbCursos.FormattingEnabled = true;
+            this.lsbCursos.Location = new System.Drawing.Point(151, 57);
+            this.lsbCursos.Name = "lsbCursos";
+            this.lsbCursos.Size = new System.Drawing.Size(474, 186);
+            this.lsbCursos.TabIndex = 1;
+            // 
+            // FrmCurso
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Controls.Add(this.lsbCursos);
+            this.Controls.Add(this.BtnListar);
+            this.Name = "FrmCurso";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -43,6 +65,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnListar;
+        private System.Windows.Forms.ListBox lsbCursos;
     }
 }
 
